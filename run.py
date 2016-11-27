66ee7846104415c69e5746eb9bb560f183447942
Matt Gaikema <mgaikema1@gmail.com>
2016-11-26 21:47:39 -0600

    add a grow method

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ cwd = os.path.dirname(os.path.realpath(__file__))
 parser = argparse.ArgumentParser(description='Run the thingy.')
 parser.add_argument('-v','--vm_dir', help='Directory for VM emulator. Defaults to ' + vm_emulator, required=False)
 parser.add_argument('-c', '--compiler_dir', help='Directory for the Jack compiler. Defaults to ' + jack_compiler, required=False)
+parser.add_argument('-r', '--run', action='store_true', help='Whether to start the emulator.', required=False)
 args = vars(parser.parse_args())
 
 if args['vm_dir']:
@@ -24,4 +25,5 @@ if args['compiler_dir']:
 	jack_compiler = args['compiler_dir']
 
 os.system("bash " + jack_compiler + " " + cwd)
-#os.system("bash " + vm_emulator + " " + cwd)
+if args['run']:
+	os.system("bash " + vm_emulator)
